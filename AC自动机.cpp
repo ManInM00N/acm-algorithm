97f002ac57a5919fc8bf62991a1d8012d7a26cbd
MoonKyandoru <124020968+MoonKyandoru@users.noreply.github.com>
2023-10-27 16:08:10 +0800

    Update AC自动机.cpp
    完善AC自动机的代码, 以及添加部分注释

--- AC自动机.cpp
+++ AC自动机.cpp
@@ -1,115 +1,60 @@
-string s[100000];
-namespace AC
-struct Trie//字典树 
-{
-    int fail;//失配指针
-    int vis[26];//子节点的位置
-    int end;//标记以这个节点结尾的单词编号 
-}AC[100000];//Trie树
-int cnt=0;//Trie的指针 
-inline void Clean(int x)
-{
-    memset(AC[x].vis,0,sizeof(AC[x].vis));
-    AC[x].fail=0;
-    AC[x].end=0;
+const int MAXN = 1e6  + 10;// 字典树最长大小 Sum(dict)
+int tr[MAXN][26];// 字典树储存容器
+int fail[MAXN];// 匹配失败时的指针
+int cnt[MAXN];.// 记录单词结束
+int idx;// 字典树末尾添加节点
+
+// 构建字典树同时初始化, 如果只有一组数据可不加
+void init(int i){
+    memset(tr[i], 0, 26 << 2);
+    fail[i] = cnt[i] = 0;
 }
-inline void Build(string s,int Num)
-{
-    int l=s.length();
-    int now=0;//字典树的当前指针 
-    for(int i=0;i<l;++i)//构造Trie树
-    {
-        if(AC[now].vis[s[i]-'a']==0)//Trie树没有这个子节点
-        {
-            AC[now].vis[s[i]-'a']=++cnt;//构造出来
-            Clean(cnt);
+
+// 构建字典树
+void insert(const string &s){
+    int u = 0;
+    for(auto i : s){
+        if(!tr[u][i - 'a']){
+            tr[u][i - 'a'] = ++idx;
+            init(idx);
         }
-        now=AC[now].vis[s[i]-'a'];//向下构造 
+        u = tr[u][i - 'a'];
     }
-    AC[now].end=Num;//标记单词结尾 
+    cnt[u] ++;
 }
-void Get_fail()//构造fail指针
-{
-    queue<int> Q;//队列 
-    for(int i=0;i<26;++i)//第二层的fail指针提前处理一下
-    {
-        if(AC[0].vis[i]!=0)
-        {
-            AC[AC[0].vis[i]].fail=0;//指向根节点
-            Q.push(AC[0].vis[i]);//压入队列 
-        }
-    }
-    while(!Q.empty())//BFS求fail指针 
-    {
-        int u=Q.front();
-        Q.pop();
-        for(int i=0;i<26;++i)//枚举所有子节点
-        {
-            if(AC[u].vis[i]!=0)//存在这个子节点
-            {
-                    AC[AC[u].vis[i]].fail=AC[AC[u].fail].vis[i];
-                        //子节点的fail指针指向当前节点的
-                        //fail指针所指向的节点的相同子节点 
-                    Q.push(AC[u].vis[i]);//压入队列 
+// 构造fail指针
+void build(){
+    queue<int> q;
+    // 遍历第一层
+    for(int i=0;i<26;i++)
+        if(tr[0][i])
+            q.push(tr[0][i]);
+    // 
+    while(!q.empty()){
+        int u = q.front();
+        q.pop();
+        for(int i=0;i<26;i++){
+            //如果字典树上拥有指向下一个字母的指针
+            if(tr[u][i]){
+                // 将下个字母匹配失败时的fail指针指向此时失败后匹配的位置
+                fail[tr[u][i]] = tr[fail[u]][i];
+                
+                q.push(tr[u][i]);
+            }else{
+                tr[u][i] = tr[fail[u]][i];
             }
-            else//不存在这个子节点 
-            AC[u].vis[i]=AC[AC[u].fail].vis[i];
-            //当前节点的这个子节点指向当
-            //前节点fail指针的这个子节点 
         }
     }
 }
-int Query(string s)//AC自动机匹配
-{
-    int l=s.length();
-    int now=0,ans=0;
-    for(int i=0;i<l;++i)
-    {
-        now=AC[now].vis[s[i]-'a'];//向下一层
-        for(int t=now;t;t=AC[t].fail)//循环求解
-                    Ans[AC[t].end].num++;
+// 查询s串中存在多少字典树中的单词
+int query(const string &s){
+    int u = 0, ans = 0;
+    for(auto i : s){
+        u = tr[u][i - 'a'];
+        for(int j = u; j && cnt[j]!=-1; j=fail[j]){
+            ans += cnt[j];
+            cnt[j] = -1;
+        }
     }
     return ans;
-}
-struct Result
-{
-    int num;
-    int pos;
-    bool operator <(Result a,Result b)
-    {
-        if(a.num!=b.num)
-            return a.num>b.num;
-        else
-            return a.pos<b.pos;
-    }
-}Ans[100000];//所有单词的出现次数 
-int main()
-{
-    int n;
-    cin>>n;
-    if(n==0)break;
-    cnt=0;
-    Clean(0);
-    for(int i=1;i<=n;++i)
-    {
-            cin>>s[i];
-            Ans[i].num=0;
-            Ans[i].pos=i;
-            Build(s[i],i);
-    }
-    AC[0].fail=0;//结束标志 
-    Get_fail();//求出失配指针
-    cin>>s[0];//文本串 
-    AC_Query(s[0]);
-    sort(&Ans[1],&Ans[n+1]);
-    cout<<Ans[1].num<<endl;
-    cout<<s[Ans[1].pos]<<endl;
-    for(int i=2;i<=n;++i)
-    {
-        if(Ans[i].num==Ans[i-1].num)
-            cout<<s[Ans[i].pos]<<endl;
-        else
-            break;
-    }
-    return 0;
 }
