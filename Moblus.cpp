c55b3be28574213082396e3fc6682e34ef1d31cc
ManInM00N <571404393@qq.com>
2023-11-29 23:16:06 +0800

    欧拉筛-莫比乌斯(欧拉)函数

--- Moblus.cpp
+++ Moblus.cpp
@@ -2,14 +2,17 @@ const int MAXN=1000000;
 bool check[MAXN+10];
 int prime[MAXN+10];
 int mu[MAXN+10];
+int phi[MAXN+10];
 void Moblus(){
     memset(check,false,sizeof(check));
+    phi[1] = 1;
     mu[1]=1;
     int tot=0;
     for(int i=2;i<=MAXN;i++){
         if(!check[i]){
             prime[tot++]=i;
             mu[i]=-1;
+            phi[i] = i-1;
         }
         for(int j=0;j<tot;j++){
             if(i*prime[j]>MAXN) 
@@ -17,9 +20,11 @@ void Moblus(){
             check[i*prime[j]]=true;
             if(i%prime[j]==0){
                 mu[i*prime[j]]=0;
+                phi[i*prime[j]] = phi[i]*prime[j];
                 break;
             }
             else{
+                phi[i*prime[j]] = phi[i]*phi[prime[j]];
                 mu[i*prime[j]]=-mu[i];
             }
         }
