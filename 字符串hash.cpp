5936d5c507eeadd310bdca8607f7099da7f6f410
ManInM00N <571404393@qq.com>
2023-12-02 22:35:27 +0800

    字符串哈希-线段树维护区间回文

--- 字符串hash.cpp
+++ 字符串hash.cpp
@@ -1,6 +1,5 @@
 const ll base = 131;
 const ll mod1=1e9+7,mod2=1e9+9;
-
 struct hash_val
 {
     vector<unsigned long long>has1,has2;
@@ -31,7 +30,98 @@ struct hash_val
 };
 hash_val a[N];
 
+ll zz[N];
+const ll P = 13331;
+const ll mod = 1e9+7;
+template<class Ty1,class Ty2> 
+inline const pair<Ty1,Ty2> operator+(const pair<Ty1, Ty2>&p1, const pair<Ty1, Ty2>&p2)//重载pair运算符
+{
+    pair<Ty1, Ty2> ret;
+    ret.first = p1.first + p2.first;
+    ret.second = p1.second + p2.second;
+    return ret;
+}
+class Segment
+{
+public:
+    struct tree
+    {
+        int l, r;
+        ll len;
+        ll h1,h2;//正逆向哈希,可求解区间内更新查询回文问题
+    };
+    int num;
+    vector<tree> tr;
+    vector<int> arr;
+    Segment(){};
+    template <class T>
+    Segment(T n) : num(n + 7)
+    {
+        tr.resize(4 * num);//线段树哈希
+        arr.resize(num, 0);
+    };
+    void pushup(ll p)
+    {
+        tr[p].h1 = (tr[rs].h1*zz[tr[ls].len]+tr[ls].h1)%mod;
+        tr[p].h2 = (tr[ls].h2*zz[tr[rs].len]+tr[rs].h2)%mod;
+    }
+    template <class T, class V, class W>
+    void build(V l, W r,T p=1ll )
+    {
+        tr[p].l = l;
+        tr[p].r = r;
+        tr[p].len = r-l+1;;
+        tr[p].h1=tr[p].h2 = arr[l];
+        if (l == r)
+            return;
+        long long mid = l + r >> 1;
+        build(l, mid,ls);
+        build(mid + 1, r,rs);
+        pushup(p);
+    }
+    template <class T, class V>
+    void update(T x, T y, V z,V p = 1ll )
+    {
+        if (tr[p].l >= x && tr[p].r <= y)
+        {
+            tr[p].h1 =tr[p].h2 = z;
+            // tr[p].lazy += z;
+            return;
+        }
+        long long mid = tr[p].l + tr[p].r >> 1;
+        // pushdown(p);
+        if (mid >= x)
+            update(x, y, z,ls);
+        if (mid < y)
+            update(x, y, z,rs);
+        pushup(p);
+    }
 
+    template <class T, class V, class W>
+    Pa query( T x, W y,V p=1ll)
+    {
+        if (tr[p].l >= x && tr[p].r <= y)
+        {
+            return Pa(zz[tr[p].l-x]*tr[p].h1%mod,zz[y-tr[p].r]*tr[p].h2%mod);
+            // return tr[p].sum;
+        }
+        long long mid = tr[p].l + tr[p].r >> 1;
+        Pa sum = {0,0};
+        if (mid >= x)
+            sum = sum + query(x, y,ls);
+        if (mid < y)
+            sum = sum + query( x, y,rs);
+        sum.first%=mod;
+        sum.second%=mod;
+        return sum;
+    }
+};
+void init(){
+    zz[0] = 1;
+    repi(i,1,n+10){
+        zz[i] = zz[i-1]*P%mod;
+    }
+}
 void print(__int128 x)//大数据输出128位int
 {
     if (!x) return ;
