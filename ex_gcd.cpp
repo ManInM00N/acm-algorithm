dc2c23877db4c6ae87417752ffdc81a9b97b13be
ManInM00N <571404393@qq.com>
2024-02-20 20:52:58 +0800

    重置

--- ex_gcd.cpp
+++ ex_gcd.cpp
@@ -1,15 +1,24 @@
-inline ll extend_gcd(ll a, ll b, ll &x, ll &y)
-{
-    /*if (a==0&&b==0){
-        return -1;
-    }*/
-    if (b == 0)
-    {
-        x = 1;
-        y = 0;
+// d = gcd(a,b)
+template <typename T = long long> long long extgcd(T a, T b, T &x, T &y) {
+    if(!b) {
+        x = 1, y = 0;
         return a;
     }
-    ll dd = extend_gcd(b, a % b, y, x);
-    y -= (a / b) * x;
-    return dd;
+    long long res = extgcd(b, a % b, y, x);
+    y -= a / b * x;
+    return res;
 }
+// ax + by = d 不存在 x,y的解时 false
+template <typename T = long long> bool extgcd(T a, T b, T &x, T &y, T d) {
+    if(!b and d % a) return false;
+    if(!b) {
+        x = d / a, y = 0;
+        return true;
+    }
+    if(extgcd(b, a % b, y, x, d)) {
+        y -= a / b * x;
+        return true;
+    }
+    return false;
+}
+
