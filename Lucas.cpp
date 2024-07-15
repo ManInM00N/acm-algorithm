d5731ccbe9da84132ce3a49f2bfa6c531810f79b
ManInM00N <571404393@qq.com>
2024-07-16 01:49:44 +0800

    调整01Trie

--- Lucas.cpp
+++ Lucas.cpp
@@ -1,3 +1,4 @@
+// clang-format off
 template<class T>
 constexpr T power(T a, ll b) {
     T res = 1;
@@ -9,7 +10,6 @@ constexpr T power(T a, ll b) {
     return res;
 }
 
-
 constexpr ll mul(ll a, ll b, ll p) {
     ll res = a * b - ll(1.L * a * b / p) * p;
     res %= p;
@@ -18,12 +18,11 @@ constexpr ll mul(ll a, ll b, ll p) {
     }
     return res;
 }
-template<class T = long long>
-struct MODINT {
+template <class T = long long> struct MODINT {
     ll x;
     constexpr MODINT() : x{} {}
     constexpr MODINT(ll x) : x{norm(x % getMod())} {}
-    
+
     static ll Mod;
     constexpr static ll getMod() {
         // if (P > 0) {
@@ -33,9 +32,7 @@ struct MODINT {
         // }
         return Mod;
     }
-    constexpr static void setMod(T Mod_) {
-        Mod = Mod_;
-    }
+    constexpr static void setMod(T Mod_) { Mod = Mod_; }
     constexpr T norm(T x) const {
         if (x < 0) {
             x += getMod();
@@ -48,12 +45,8 @@ struct MODINT {
     // constexpr T &operator() ()const{
     //     return val();
     // }
-    constexpr T val() const {
-        return x;
-    }
-    explicit constexpr operator T() const {
-        return x;
-    }
+    constexpr T val() const { return x; }
+    explicit constexpr operator T() const { return x; }
     constexpr MODINT operator-() const {
         MODINT res;
         res.x = norm(getMod() - x);
@@ -63,21 +56,19 @@ struct MODINT {
         assert(x != 0);
         return power(*this, getMod() - 2);
     }
-    constexpr MODINT &operator*=(MODINT rhs) & {
+    constexpr MODINT& operator*=(MODINT rhs) & {
         x = mul(x, rhs.x, getMod());
         return *this;
     }
-    constexpr MODINT &operator+=(MODINT rhs) & {
+    constexpr MODINT& operator+=(MODINT rhs) & {
         x = norm(x + rhs.x);
         return *this;
     }
-    constexpr MODINT &operator-=(MODINT rhs) & {
+    constexpr MODINT& operator-=(MODINT rhs) & {
         x = norm(x - rhs.x);
         return *this;
     }
-    constexpr MODINT &operator/=(MODINT rhs) & {
-        return *this *= rhs.inv();
-    }
+    constexpr MODINT& operator/=(MODINT rhs) & { return *this *= rhs.inv(); }
     friend constexpr MODINT operator*(MODINT lhs, MODINT rhs) {
         MODINT res = lhs;
         res *= rhs;
@@ -98,13 +89,14 @@ struct MODINT {
         res /= rhs;
         return res;
     }
-    friend constexpr std::istream &operator>>(std::istream &is, MODINT &a) {
+    friend constexpr std::istream& operator>>(std::istream& is, MODINT& a) {
         ll v;
         is >> v;
         a = MODINT(v);
         return is;
     }
-    friend constexpr std::ostream &operator<<(std::ostream &os, const MODINT &a) {
+    friend constexpr std::ostream& operator<<(std::ostream& os,
+                                              const MODINT& a) {
         return os << a.val();
     }
     friend constexpr bool operator==(MODINT lhs, MODINT rhs) {
@@ -114,8 +106,7 @@ struct MODINT {
         return lhs.val() != rhs.val();
     }
 };
-template<>
-ll MODINT<ll>::Mod = 998244353;
+template <> ll MODINT<ll>::Mod = 998244353;
 using Z = MODINT<ll>;
 
 /*
@@ -125,58 +116,71 @@ using Z = MODINT<ll>;
     1e9 + 9
 */
 // class Combine
-namespace combine
+namespace combine {
+using ll = long long;
+ll p = Z::Mod;
+const int MAXN = 2e6 + 5;
+Z fact[MAXN];
+Z inv[MAXN]; // 逆元
+// Z B[MAXN]; // 伯努利数,求解自然数幂次数列求和 eg. S_{m}(n) = 1^m + 2^m + ...
+// + n^m
+inline Z C(ll m, ll n);
+void Init() // 组合数初始化；
 {
-    ll p = Z::Mod;
-    Z fact[2000005];
-    void Init() // 组合数初始化；
-    {
-        // inv[1] = 1;
-        // for (int i = 2; i <= n; i++)
-        // {
-        //     inv[i] = (m- m/i) * inv[m%i] % m;
-        // }//在模数为合数时线性求逆元
-        fact[0] = 1;
-        for (int i = 1; i <= 2000004; ++i)
-            fact[i] = fact[i - 1] * i;
-    }
-    //从 m 个数中选出 n 个数
-    inline Z C(ll m, ll n)
-    { // 求组合
-        return m < n ? 0ll : (fact[m] / fact[n] / fact[m - n]);
-    }
-    //从 m 个数中选出 n 个数
-    inline Z Lucas(ll m, ll n)
-    { // 卢卡斯定理
-        return n == 0 ? 1ll : C(m % p, n % p) * Lucas(m / p, n / p);
-    }
-    //从 m 个数中按顺序取 n 个数
-    inline Z P(ll m, ll n)//
-    {
-        return fact[m] / fact[m - n];
-    }
-    // Catalan 卡特兰数列
-    inline Z H(ll n){
-        return C(n*2,n)-C(n*2,n-1);
-    }
-    // 第二类斯特林数 表示将 m 个两两不同的元素，划分为 n 个互不区分的非空子集的方案数
-    inline Z Second_Stirling(ll m,ll n){
-        Z ans = 0;
-        ll base = (n%2==1)?-1:1;
-        for (int i = 0; i <= n; i++)
-        {
-            ans += base * power(Z(i), m) / fact[i] / fact[n - i];
-            base*=-1; 
-        }
-        // for (int i = 0; i <=n; i++)
-        // {
-        //     ans+= base*C(n,i)*power(Z(i),m);
-        //     base*=-1;
-        //     /* code */
-        // }
-        // ans/=fact[n];
-        return ans;
-    }
+    inv[1] = 1;
+    for (int i = 2; i < MAXN; i++) {
+        // inv[i] = (m- m/i) * inv[m%i] % m;
+        inv[i] = (Z::Mod - Z::Mod / i) * inv[Z::Mod % i];
+    } // 在模数为合数时线性求逆元
+    fact[0] = 1;
+    for (int i = 1; i < MAXN; ++i)
+        fact[i] = fact[i - 1] * i;
+    // B[0] = 1;
+    // for(int i =1;i<MAXN;i++){
+    //     Z ans  =0 ;
+    //     if (i==MAXN-1){
+    //         break;
+    //     }
+    //     for(int k = 0;k<i;i++){
+    //         ans += C(i+1,k)*B[k];
+    //     }
+    //     ans = ans + (-inv[i+1]);
+    //     B[i] = ans;
+    // }
 }
+// 从 m 个数中选出 n 个数
+inline Z C(ll m, ll n) { // 求组合
+    return m < n ? 0ll : (fact[m] / fact[n] / fact[m - n]);
+}
+// 从 m 个数中选出 n 个数
+inline Z Lucas(ll m, ll n) { // 卢卡斯定理
+    return n == 0 ? 1ll : C(m % p, n % p) * Lucas(m / p, n / p);
+}
+// 从 m 个数中按顺序取 n 个数
+inline Z P(ll m, ll n) //
+{
+    return fact[m] / fact[m - n];
+}
+// Catalan 卡特兰数列
+inline Z H(ll n) { return C(n * 2, n) - C(n * 2, n - 1); }
+// 第二类斯特林数 表示将 m 个两两不同的元素，划分为 n
+// 个互不区分的非空子集的方案数
+inline Z Second_Stirling(ll m, ll n) {
+    Z ans = 0;
+    ll base = (n % 2 == 1) ? -1 : 1;
+    for (int i = 0; i <= n; i++) {
+        ans += base * power(Z(i), m) / fact[i] / fact[n - i];
+        base *= -1;
+    }
+    // for (int i = 0; i <=n; i++)
+    // {
+    //     ans+= base*C(n,i)*power(Z(i),m);
+    //     base*=-1;
+    //     /* code */
+    // }
+    // ans/=fact[n];
+    return ans;
+}
+} // namespace combine
 using namespace combine;
-
+// clang-format on
--- Trie.cpp
+++ Trie.cpp
@@ -1,108 +1,148 @@
-template <class T = int >
-class Trie01{
-public:
-    struct node{
-        T dir[2];
-        ll cnt[2];
+// clang-format off
+template <class T = int> class Trie01 {
+  public:
+    struct node {
+        vector<T> dir;
+        // ll dir[2];
     };
     std::vector<node> nex;
+    std::vector<T> cnt;
     T idx;
-    T varity = 2;
-    Trie01(){};
-    // 总体数量和字典种类
-    Trie01(T siz, T varity = 2):varity(varity){
-        idx =0 ;
-        nex.resize(1);
-        // cnt.resize(1);
-        // cnt[0] = INF;
-    }
-    void init(){
+    T varity;
+    // T static base;
+    Trie01() { // base = 0;
+    };
+    // 总体字典种类
+    Trie01(T varity) : varity(varity) {
         idx = 0;
         nex.clear();
-        nex.resize(1);
+        nex.push_back({vector<T>(varity)});
+        // nex.resize(1);
+        cnt.resize(1);
+        // base = 0;
+        // cnt[0] = INF;
     }
-    void insert(T num){
+    void init() {
+        idx = 0;
+        nex.clear();
+        nex.push_back({vector<T>(varity)});
+        // nex.resize(1);
+        cnt.resize(1);
+    }
+    void insert(T num, T u = 1) {
         int p = 0;
-        for (int i = 29; i >=0; i--)
-        {
-            T type = (num>>i) & 1;
-            if (!nex[p].dir[type]){
+        for (int i = 30; i >= 0; i--) {
+            T type = (num >> i) & 1;
+            if (!nex[p].dir[type]) {
                 nex[p].dir[type] = ++idx;
-                nex.push_back({{0,0},{0,0}});
+                nex.push_back({vector<T>(varity)});
+                cnt.push_back(0);
             }
-            nex[p].cnt[type]++;
             p = nex[p].dir[type];
-            // cnt[p]= min(cnt[p],rr);
+            cnt[p]++;
+            // cnt[p] = max(cnt[p], u);//小于等于这部分的最大值
+            // cnt[p]= min(cnt[p],u);
         }
     }
-    bool find( T num, ll rr ){
+    bool find(T num) {
         T p = 0;
-        ll res= 0;
-        bool ok =0;
-        for (int i = 29; i >=0; i--){
-            T type = (num>>i)&1;
-            T tt = (k>>i)&1;
-            if (tt==0){
-                if (nex[p].dir[type^1]){
-                    ok |= (rr<=nex[p].cnt[type ^ 1]);
+        for (int i = 30; i >= 0; i--) {
+            T type = num >> i & 1;
+            p = nex[p].dir[type];
+            if (!p) {
+                return 0;
+            }
+        }
+        return p != 0;
+    }
+    // 查找比num^tt的数的个数
+    // bool find(T num, ll rr) {
+    //     T p = 0;
+    //     ll res = 0;
+    //     bool ok = 0;
+    //     for (int i = 30; i >= 0; i--) {
+    //         T type = (num >> i) & 1;
+    //         T tt = (k >> i) & 1;
+    //         if (tt == 0) {
+    //             if (nex[p].dir[type ^ 1]) {
+    //                 ok |= (rr <= cnt[nex[p].dir[type ^ 1]]);
+    //             }
+    //         }
+    //         p = nex[p].dir[type ^ tt];
+    //         if (!p) {
+    //             return ok;
+    //         }
+    //     }
+    //     return ok;
+    // }
+    T find(T num, ll rr) {
+
+        T p = 0;
+        ll res = 0;
+        for (int i = 30; i >= 0; i--) {
+            T type = (num >> i) & 1;
+            T tt = (rr >> i) & 1;
+            if (tt == 0) {
+                if (nex[p].dir[type ^ 1]) {
+                    res += cnt[nex[p].dir[type ^ 1]];
                 }
+                p = nex[p].dir[type];
+            } else {
+                p = nex[p].dir[type ^ 1];
             }
-            p = nex[p].dir[type^tt];
-            if (!p){
-                return ok;
+            if (!p) {
+                return res;
             }
-            // p = nex[p][type];
         }
-        return ok;
+        // res+= cnt[p]; // 考虑是否等于
+        return res;
     }
-    void del(T num){
+    void del(T num) {
         T p = 0;
-        for (int i = 30; i >= 0; i--){
-            T type = (num>>i)&1;
-            nex[p].cnt[type]--;
+        for (int i = 30; i >= 0; i--) {
+            T type = (num >> i) & 1;
+            /*nex[p].cnt[type]--;*/
             p = nex[p].dir[type];
+            assert("Not exist numer");
+            cnt[p]--;
         }
     }
-    T find_rank(T num){
+    T find_rank(T num) {
         T p = 0;
         T sum = 0;
-        for (int i = 30; i >= 0; i--){
-            T type = (num>>i)&1;
-            if ( type==1&&nex[p].dir[type^1] ){   
-                sum+= nex[p].cnt[type^1];
+        for (int i = 30; i >= 0; i--) {
+            T type = (num >> i) & 1;
+            if (type == 1 && nex[p].dir[type ^ 1]) {
+                sum += cnt[nex[p].dir[type ^ 1]];
             }
-            if (!nex[p].dir[type]){
+            if (!nex[p].dir[type]) {
                 break;
             }
             p = nex[p].dir[type];
         }
         return sum;
     }
-    T find_by_rank(T num){
+    T find_by_rank(T num) {
         T p = 0;
         T ans = 0;
         T sum = 0;
-        for (int i = 30; i >= 0; i--){
+        for (int i = 30; i >= 0; i--) {
             T type = 1;
-            if (nex[p].cnt[type^1]+sum<num){
-                sum+=nex[p].cnt[type^1];
+            if (cnt[nex[p].dir[type ^ 1]] + sum < num) {
+                sum += cnt[nex[p].dir[type ^ 1]];
                 p = nex[p].dir[type];
-                ans+= 1ll<<i;
-            }else {
-                p = nex[p].dir[type^1];
+                ans += 1ll << i;
+            } else {
+                p = nex[p].dir[type ^ 1];
             }
-            if (!p){
+            if (!p) {
                 break;
             }
         }
         return ans;
     }
-    T pre(T num){
-        return find_by_rank(find_rank(num));
-    }
-    T nxt(T num){
-        return find_by_rank(find_rank(num+1)+1);
-    }
+    T pre(T num) { return find_by_rank(find_rank(num)); }
+    T nxt(T num) { return find_by_rank(find_rank(num + 1) + 1); }
 };
 template <class T = int >
 class Trie{
@@ -138,3 +178,4 @@ public:
         return 1;
     }
 };
+// clang-format on
