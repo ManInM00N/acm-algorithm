9247eb2f7c9583e8364cf290c9561f3c0a342430
czp <571404393@qq.com>
2024-08-01 11:43:22 +0800

    debug

--- DynamicSegmentTree.cpp
+++ DynamicSegmentTree.cpp
@@ -2,14 +2,10 @@
 class info {
   public:
     int l = 0, r = 0;
-    ll val = 0, sum = 0, lazy = 0;
-    ll ld = 0, rd = 0;
+    ll  sum = 0, lazy = 0;
     info operator+(const info& b) const {
         info a = *this, c;
-        c.ld = a.ld;
-        c.rd = b.rd;
         c.sum = a.sum + b.sum;
-        c.val = a.val + b.val;
         return c;
     }
 };
@@ -23,7 +19,7 @@ class DynamicSegmentTree {
     int dir;
     DynamicSegmentTree(int _) : num(_) {
         dir = 0;
-        tr.resize((num + 1) << 6);
+        tr.resize((num + 1) * 50);
         rt.resize(num + 1, 0);
     }
     inline int AddNode(int p) {
@@ -41,37 +37,32 @@ class DynamicSegmentTree {
         return dir;
     }
     void pushup(int p) { tr[p].sum = tr[ls].sum + tr[rs].sum; }
-    void pushdown(int p) {
-        if (tr[p].ld == tr[p].rd) {
-            return;
-        }
-        ll mid = tr[p].ld + tr[p].rd >> 1;
+    void pushdown(int p,ll ld,ll rd) {
+        ll mid = ld + rd >> 1;
         if (!ls)
-            ls = AddNode({0, 0, 0, 0, 0, tr[p].ld, mid});
+            ls = AddNode({0, 0, 0,});
         if (!rs)
-            rs = AddNode({0, 0, 0, 0, 0, mid + 1, tr[p].rd});
-        if (tr[p].lazy != -1) {
-            tr[ls].sum = tr[p].lazy * (tr[ls].rd - tr[ls].ld + 1);
-            tr[rs].sum = tr[p].lazy * (tr[rs].rd - tr[rs].ld + 1);
+            rs = AddNode({0, 0, 0,});
+        if (tr[p].lazy != 0) {
+            tr[ls].sum = tr[p].lazy * (mid-ld+1);
+            tr[rs].sum = tr[p].lazy * (rd-mid);
             tr[ls].lazy = tr[p].lazy;
             tr[rs].lazy = tr[p].lazy;
         }
-        tr[p].lazy = -1;
+        tr[p].lazy = 0;
     }
     int Modify(int p, ll ld, ll rd, ll ql, ll qr, ll z) {
         if (!p) {
             p = AddNode(p);
-            tr[p].ld = ld;
-            tr[p].rd = rd;
             tr[p].sum = rd - ld + 1;
-            tr[p].lazy = -1;
+            tr[p].lazy = 0;
         }
         if (ql <= ld && rd <= qr) {
             tr[p].sum = z * (rd - ld + 1);
             tr[p].lazy = z;
             return p;
         }
-        pushdown(p);
+        pushdown(p,ld,rd);
         ll mid = ld + rd >> 1;
         if (ql <= mid) {
             tr[p].l = Modify(tr[p].l, ld, mid, ql, qr, z);
@@ -88,18 +79,20 @@ class DynamicSegmentTree {
         if (!b)
             return a;
         if (l == r) {
+            tr[a].sum += tr[b].sum;
             return a;
         }
         ll mid = l + r >> 1;
         tr[a].l = Merge(tr[a].l, tr[b].l, l, mid);
         tr[a].r = Merge(tr[a].r, tr[b].r, mid + 1, r);
+        pushup(a);
         return a;
     }
     info Query(int p, ll ld, ll rd, ll ql, ll qr) {
         if (ql <= ld && rd <= qr) {
             return tr[p];
         }
-        pushdown(p);
+        pushdown(p,ld,rd);
         info res;
         ll mid = ld + rd >> 1;
         if (ql <= mid) {
@@ -120,3 +113,4 @@ class DynamicSegmentTree {
     使用seg.rt[0] = seg.Modify(seg.rt[0],1,n,x,y,z);
  */
 // clang-format on
+
