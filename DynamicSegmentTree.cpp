923ab3a30827052bfcc20462ce84ede649cb4842
ManInM00N <571404393@qq.com>
2024-08-05 02:19:24 +0800

    update

--- DynamicSegmentTree.cpp
+++ DynamicSegmentTree.cpp
@@ -50,10 +50,10 @@ class DynamicSegmentTree {
     void pushup(int p) { tr[p].sum = tr[ls].sum + tr[rs].sum; }
     void pushdown(int p,ll ld,ll rd) {
         ll mid = ld + rd >> 1;
-        if (!ls)
-            ls = AddNode({0, 0, 0,});
-        if (!rs)
-            rs = AddNode({0, 0, 0,});
+        // if (!ls)
+        //     ls = AddNode({0, 0, 0,});
+        // if (!rs)
+        //     rs = AddNode({0, 0, 0,});
         if (tr[p].lazy != 0) {
             tr[ls].sum = tr[p].lazy * (mid-ld+1);
             tr[rs].sum = tr[p].lazy * (rd-mid);
@@ -65,12 +65,10 @@ class DynamicSegmentTree {
     int Modify(int p, ll ld, ll rd, ll ql, ll qr, ll z) {
         if (!p) {
             p = AddNode(p);
-            tr[p].sum = rd - ld + 1;
-            tr[p].lazy = 0;
         }
         if (ql <= ld && rd <= qr) {
-            tr[p].sum = z * (rd - ld + 1);
-            tr[p].lazy = z;
+            tr[p].sum+=z; 
+            tr[p].lazy += z;
             return p;
         }
         pushdown(p,ld,rd);
@@ -84,30 +82,46 @@ class DynamicSegmentTree {
         pushup(p);
         return p;
     }
-    int Merge(int a, int b, int l, int r) {
-        if (!a)
-            return b;
-        if (!b)
-            return a;
-        if (l == r) {
+    int Merge(int a, int b, int ld, int rd) {
+        if (!a || !b)
+            return a + b;
+        if (ld == rd) {
             tr[a].sum += tr[b].sum;
             tr[b] = info();
             cyc.push(b);
             return a;
         }
-        ll mid = l + r >> 1;
+
+        ll mid = ld + rd >> 1;
+        pushdown(a,ld,rd);
+        pushdown(b,ld,rd);
         /*
             计算两段区间相互作用可以造成的贡献
             例如计算逆序对数量  
             交换区间: tr[tr[a].l].sum * tr[tr[b].r].sum 
             不交换: tr[tr[a].r].sum + tr[tr[b].l].sum
         */
-        tr[a].l = Merge(tr[a].l, tr[b].l, l, mid);
-        tr[a].r = Merge(tr[a].r, tr[b].r, mid + 1, r);
+        tr[a].l = Merge(tr[a].l, tr[b].l, ld, mid);
+        tr[a].r = Merge(tr[a].r, tr[b].r, mid + 1, rd);
         pushup(a);
         tr[b] = info();
         cyc.push(b);
         return a;
+    }
+    // 注意:此处函数为可持久化处理,即保存子树信息,mle的可能非常大!!!
+    int merge(int a, int b, int ld, int rd) {
+        if (!a || !b)
+            return a + b;
+        int p = AddNode(0);
+        if (ld == rd) {
+            tr[p].sum = min(tr[a].sum, tr[b].sum);
+            return p;
+        }
+        ll mid = ld + rd >> 1;
+        tr[p].l = merge(tr[a].l, tr[b].l, ld, mid);
+        tr[p].r = merge(tr[a].r, tr[b].r, mid + 1, rd);
+        pushup(p);
+        return p;
     }
     info Query(int p, ll ld, ll rd, ll ql, ll qr) {
         if (ql <= ld && rd <= qr) {
--- Persistant_Segment_Tree主席树.cpp
+++ Persistant_Segment_Tree主席树.cpp
@@ -1,101 +1,119 @@
-template<class T = long long>
-class Persistant_Segment_Tree{
-public:
-    constexpr static T maxn = 1e5+7;
-    //与常规线段树不同  主席树结构体的l,r是对树的索引而不是区间的索引！！
-    //而函数中的参数l,r则是对应的区间
-    struct tree
-    {
-        T l, r,val;
-    };
-    vector<tree> tr;
-    vector<T> arr,root;//root[i]为第 i 个版本的根节点编号
-    T dir,siz; // dir为节点数  siz为主席树范围
-    Persistant_Segment_Tree(T n = maxn) : siz(n+7){
+class info {
+  public:
+    int l = 0, r = 0;
+    int val = INF;
+    info operator+(const info& b) const {
+        info a = *this, c;
+        c.val = min(a.val, b.val);
+        return c;
+    }
+};
+template <class T = long long> class Persistant_Segment_Tree {
+  public:
+    constexpr static int maxn = 1e5 + 7;
+    // 与常规线段树不同  主席树结构体的l,r是对树的索引而不是区间的索引！！
+    // 而函数中的参数ld,rd则是对应的区间
+    vector<info> tr;
+    vector<int> arr, rt; // root[i]为第 i 个版本的根节点编号
+    int dir, num;        // dir为节点数  siz为主席树范围
+    Persistant_Segment_Tree(int n = maxn) : num(n + 7) {
         dir = 0;
-        arr.resize(siz);
-        root.resize(siz);
-        tr.resize(siz<<5);
-    } 
-    T Build(T l,T r,T p = 0){
+        arr.resize(num, INF);
+        rt.resize(num);
+        tr.resize(num << 5);
+    }
+    int Build(int l, int r, int p = 0) {
         p = ++dir;
-        if (l==r){
+        if (l == r) {
             tr[p].val = arr[l];
             return p;
         }
-        T mid = l+r>>1;
+        T mid = l + r >> 1;
         tr[p].l = Build(l, mid, tr[p].l);
         tr[p].r = Build(mid + 1, r, tr[p].r);
-        // pushup(p);
+        pushup(p);
         return p;
     }
-    inline T copy(T p){
+    inline int copy(int p) {
         ++dir;
         tr[dir] = tr[p];
-
         // tr[dir].val ++ ;  //区间第k值的前缀和累加  若为单点历史修改则不需要
-        return dir; 
+        return dir;
     }
-    constexpr inline T GetHead(T p){
-        return root[p];
-    }
-    void pushup(T p){
-        tr[p].val = tr[tr[p].l].val+tr[tr[p].r].val;
-    }
-    T Modify(T l, T r, T pos, T p )//区间第k值的更新  去掉val即可
+    constexpr inline int GetHead(int p) { return rt[p]; }
+    void pushup(int p) { tr[p].val = min(tr[tr[p].l].val, tr[tr[p].r].val); }
+    int Modify(int p, ll ld, ll rd, ll pos) // 区间第k值的更新  去掉val即可
     {
         p = copy(p);
-        if (l==r){
+        if (ld == rd) {
             return p;
         }
-        T mid = l+r>>1;
+        T mid = ld + rd >> 1;
         if (pos <= mid) {
-            tr[p].l = Modify(l,mid,pos,tr[p].l);
-        }else {
-            tr[p].r = Modify(mid+1,r,pos,tr[p].r);
+            tr[p].l = Modify(tr[p].l, ld, mid, pos);
+        } else {
+            tr[p].r = Modify(tr[p].r, mid + 1, rd, pos);
         }
         // pushup(p);
         return p;
     }
-    T Modify(T l, T r, T pos, T val, T p )//历史版本单点修改 
+    int Modify(int p, int ld, int rd, int pos, int val) // 历史版本单点修改
     {
         p = copy(p);
-        if (l==r){
-            tr[p].val = val;  //版本修改
+        if (ld == rd) {
+            tr[p].val = min(tr[p].val, val); // 版本修改
             return p;
         }
-        T mid = l+r>>1;
+        T mid = ld + rd >> 1;
         if (pos <= mid) {
-            tr[p].l = Modify(l,mid,pos,val,tr[p].l);
-        }else {
-            tr[p].r = Modify(mid+1,r,pos,val,tr[p].r);
+            tr[p].l = Modify(tr[p].l, ld, mid, pos, val);
+        } else {
+            tr[p].r = Modify(tr[p].r, mid + 1, rd, pos, val);
         }
         // pushup(p);
         return p;
     }
-    T Query(T l,T r,T pos, T p){
-        if (l==r){
+    T Query(T ld, T rd, T pos, T p) {
+        if (ld == rd) {
             return tr[p].val;
         }
-        T mid = l+r >>1;
-        if (pos<=mid){
-            return Query(l,mid,pos, tr[p].l);
-        }else{
-            return Query(mid + 1, r, pos, tr[p].r);
-        } 
-    }
-    T Query_Kth(T l,T r, T vl,T vr, T k){
-        if (l==r){
-            return l;
-        }   
-        T mid = l+r>>1;
-        T lsum = tr[tr[vr].l].val-tr[tr[vl].l].val;
-        if (lsum>=k){
-            return Query_Kth(l,mid,tr[vl].l,tr[vr].l,k);
-        }else {
-            return Query_Kth(mid+1,r,tr[vl].r,tr[vr].r,k-lsum);
+        T mid = ld + rd >> 1;
+        if (pos <= mid) {
+            return Query(tr[p].l, ld, mid, pos);
+        } else {
+            return Query(tr[p].r, mid + 1, rd, pos);
         }
+    }
+    T Query_Kth(T ld, T rd, T ql, T qr, T k) {
+        if (ld == rd) {
+            return ld;
+        }
+        T mid = ld + rd >> 1;
+        T lsum = tr[tr[qr].l].val - tr[tr[ql].l].val;
+        if (lsum >= k) {
+            return Query_Kth(ld, mid, tr[ql].l, tr[qr].l, k);
+        } else {
+            return Query_Kth(mid + 1, rd, tr[ql].r, tr[qr].r, k - lsum);
+        }
+    }
+    info Query(int p, ll ld, ll rd, int ql, int qr) {
+        if (ql <= ld && rd <= qr) {
+            return tr[p];
+        }
+        info res;
+        ll mid = ld + rd >> 1;
+        if (ql <= mid) {
+            res = Query(tr[p].l, ld, mid, ql, qr);
+            if (qr > mid) {
+                res = res + Query(tr[p].r, mid + 1, rd, ql, qr);
+            }
+
+        } else {
+            res = Query(tr[p].r, mid + 1, rd, ql, qr);
+        }
+        return res;
     }
 };
 // 最初的版本p默认为0  -> seg.root[0] = seg.build(l,r)
 // 更新的版本 ： root[i] = modify(1,n,pos,val, root[x])
+// 选取rt [查询] 的时候,切记不要使用未被使用的节点
