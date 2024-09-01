b66c730c797d3006079b8864d04d1c6ba396f81a
ManInM00N <571404393@qq.com>
2024-09-01 17:47:45 +0800

    before ccpc
    debug

--- Prim.cpp
+++ Prim.cpp
@@ -1,24 +1,30 @@
-void prim()
-{
-    dist[1] = 0;//把点1加入集合S，点1在集合S中，将它到集合的距离初始化为0
-    book[1] = true;//表示点1已经加入到了S集合中
-    for(int i = 2 ; i <= n ;i++)dist[i] = min(dist[i],g[1][i]);//用点1去更新dist[]
-    for(int i = 2 ; i <= n ; i++)
-    {
-        int temp = INF;//初始化距离
-        int t = -1;//接下来去寻找离集合S最近的点加入到集合中，用t记录这个点的下标。
-        for(int j = 2 ; j <= n; j++)
-        {
-            if(!book[j]&&dist[j]<temp)//如果这个点没有加入集合S，而且这个点到集合的距离小于temp就将下标赋给t
-            {
-                temp = dist[j];//更新集合V到集合S的最小值
-                t = j;//把点赋给t
+auto prim = [&](ll st) -> ll {
+    dis[st] = 0;
+    priority_queue<node> q;
+    q.push({0, st});
+    ll res = 0;
+    while (!q.empty()) {
+        node tt = q.top();
+        q.pop();
+        if (in[tt.to])
+            continue;
+        in[tt.to] = 1;
+        res++;
+        sum += tt.w;
+        for (int i = h[tt.to]; ~i; i = e[i].next) {
+            ll v = e[i].to;
+            ll w = e[i].w;
+            if (!is[v])
+                continue;
+
+            if (dis[v] > w) {
+                dis[v] = w;
+                q.push({dis[v], v});
             }
         }
-        if(t==-1){res = INF ; return ;}
-        //如果t==-1，意味着在集合V找不到边连向集合S，生成树构建失败，将res赋值正无穷表示构建失败，结束函数
-        book[t] = true;//如果找到了这个点，就把它加入集合S
-        res+=dist[t];//加上这个点到集合S的距离
-        for(int j = 2 ; j <= n ; j++)dist[j] = min(dist[j],g[t][j]);//用新加入的点更新dist[]
     }
-}
+    if (res != num) {
+        sum = -1; // sum为外部变量,表示最小花费
+    }
+    return res;
+};
